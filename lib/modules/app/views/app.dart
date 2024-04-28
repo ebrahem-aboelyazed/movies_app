98b1f7894e1eae58faa67a355bf938ec640db02e
Ibrahim Aboelyazed <ebrahem6076@gmail.com>
2024-04-28 14:17:00 +0300

    Added movies module

--- lib/modules/app/views/app.dart
+++ lib/modules/app/views/app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:movies_app/core/core.dart';
 import 'package:movies_app/l10n/l10n.dart';
+import 'package:movies_app/modules/movies/movies.dart';
 import 'package:movies_app/routes/routes.dart';
 
 class App extends StatelessWidget {
@@ -10,13 +11,20 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
-      providers: [],
+      providers: [
+        BlocProvider<MoviesCubit>(
+          create: (BuildContext context) {
+            return MoviesCubit(
+              moviesService: getIt.get<MoviesService>(),
+            );
+          },
+        ),
+      ],
       child: GestureDetector(
         onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
         child: MaterialApp.router(
           routerConfig: getIt.get<AppPages>().router,
           debugShowCheckedModeBanner: false,
-          //theme: hasLightTheme ? lightTheme : darkTheme,
           localizationsDelegates: AppLocalizations.localizationsDelegates,
           supportedLocales: AppLocalizations.supportedLocales,
         ),
